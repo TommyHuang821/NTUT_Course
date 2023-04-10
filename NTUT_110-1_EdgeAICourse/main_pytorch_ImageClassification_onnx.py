7b3fc52515afccbdf1c7dae0a609a6b31f3716c3
Chih-Sheng (Tommy) Huang <chih.sheng.huang821@gmail.com>
2023-04-10 15:15:14 +0800

    upload week08 dimension reduction

--- NTUT_110-1_EdgeAICourse/main_pytorch_ImageClassification_onnx.py
+++ NTUT_110-1_EdgeAICourse/main_pytorch_ImageClassification_onnx.py
@@ -31,7 +31,7 @@ def main():
                                trns.ToTensor(), 
                                trns.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
 
     while True:
         ret, img = cap.read()
